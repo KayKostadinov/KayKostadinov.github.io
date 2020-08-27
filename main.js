7d87a330c0a0d29012cc12bac51cdca5e5f7d153
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-08-27 15:01:36 +0300

    Progressive hero image

---
+++ main.js
@@ -0,0 +1,6 @@
+const heroImg = document.querySelector('.welcome');
+
+heroImg.onload = () => {
+    heroImg.style.backgroundImage = "linear-gradient(rgba($primary, 1) 0%, rgba(transparent, 0) 20%, rgba($teal, 0.2) 80%, rgba($primary, 1) 100%),url('../pictures/55.jpg')"
+}
+console.log(heroImg)
