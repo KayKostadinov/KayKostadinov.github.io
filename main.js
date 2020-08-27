1ffff469527ed8f01f501520dab51fb56a5c0ff7
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-08-27 15:11:17 +0300

    More img optimizations

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const heroImg = document.querySelector('.welcome');
 
-heroImg.onload = () => {
-    heroImg.style.backgroundImage = "linear-gradient(rgba($primary, 1) 0%, rgba(transparent, 0) 20%, rgba($teal, 0.2) 80%, rgba($primary, 1) 100%),url('../pictures/55.jpg')"
+let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+console.log(width)
+if (width < 900) {
+    heroImg.onload = () => {
+        heroImg.style.backgroundImage = "linear-gradient(rgba($primary, 1) 0%, rgba(transparent, 0) 20%, rgba($teal, 0.2) 80%, rgba($primary, 1) 100%),url('../pictures/55.jpg')"
+    }
 }
